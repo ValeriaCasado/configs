8b7aba8aa8feecf0e1449e2d757a821ca9a3bc01
valeria <vcasadoh@gmail.com>
2024-11-01 16:23:41 +0000

    Update install

--- install.sh
+++ install.sh
@@ -11,9 +11,22 @@ brew install neovim
 mkdir ~/.config
 mkdir ~/.config/nvim
 
-mv init.lua ~/.config/nvim
+cp init.lua ~/.config/nvim
 
 mkdir ~/.config/nvim/lua
 mkdir ~/.config/nvim/lua/plugins
 
 echo 'if true then return {} end' >> ~/.config/nvim/lua/plugins/example.lua
+
+# Use iTerm because terminal sux
+DIR='/Applications/iTerm.app'
+if test -d "$DIR"; then
+    echo "$DIR exists."
+else
+    curl --output out.zip https://iterm2.com/downloads/stable/iTerm2-3_5_5.zip
+    unzip out.zip 
+    mv  iTerm.app /Applications
+    open  /Applications/iTerm.app
+    rm out.zip
+fi
+
